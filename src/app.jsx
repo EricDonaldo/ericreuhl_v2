85a3c36ecd8c302c3255e277b70106be7f75e1d5
EricDonaldo <eric.reuhl2802@gmx.de>
2026-01-27 11:36:28 +0100

    Feat: Navbar routes completed

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,9 @@
 import { Routes,Route } from "react-router";
 import Navbar from "./components/navbar";
 import Home from "./routes/home";
+import About from "./routes/about";
+import Contact from "./routes/contact";
+import Projects from "./routes/projects"
 
 export default function App() {
   return (
@@ -9,6 +12,9 @@ export default function App() {
             <Navbar />
             <Routes>
               <Route path="/" element={<Home />}/>
+              <Route path="/about" element={<About />}/>
+              <Route path="/contact" element={<Contact />}/>
+              <Route path="/projects" element={<Projects />}/>
             </Routes>
         </div>
     </>
--- src/routes/projects.jsx
+++ src/routes/projects.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const projects = () => {
   return (
-    <div>projects</div>
+    <div className="min-h-screen bg-slate-50 w-full flex items-center justify-center">
+        <div className="flex flex-col items-left max-w-3xl gap-5">
+            <h1 className="text-6xl font-bold text-black text-center mt-20">Projects</h1>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p> 
+        </div>
+    </div>
   )
 }
 
