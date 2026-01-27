b208b70ae10175546f99eb149af86cf7412516b8
EricDonaldo <eric.reuhl2802@gmx.de>
2026-01-27 10:40:41 +0100

    Setup: React, React Router setup completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,12 @@
-import './style.css'
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./app";
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
