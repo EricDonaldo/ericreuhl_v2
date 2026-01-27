4f562c1b80f1e63e251b8e29c789bba700639253
EricDonaldo <eric.reuhl2802@gmx.de>
2026-01-27 11:31:47 +0100

    Feat: Home Route configured

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,15 @@
+import { Routes,Route } from "react-router";
 import Navbar from "./components/navbar";
+import Home from "./routes/home";
 
 export default function App() {
   return (
     <>
         <div className='min-h-screen flex flex-col bg-gray-50'>
             <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />}/>
+            </Routes>
         </div>
     </>
   );
